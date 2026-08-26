9df048682769f98e83b1e39d629199ef73430b19
kvaraprasadmbbs2-jpg <kvaraprasadmbbs2@gmail.com>
2026-08-26 18:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ height = st.number_input("Enter your height in mt",min_value = 0.5)
 if st.button("calculate bmi"):
   bmi = weight / (height * height)
   st.write("Your bmi is = ", round (bmi,2))
-if bmi < 18.5:
+ if bmi < 18.5:
   st.write("bmi category - Underweight")
 elif bmi < 25:
   st.write ("bmi categoery - Normal")
