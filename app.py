fb5545fac0d0bcd86cbfbc3d50fb87d99bbb8c01
kvaraprasadmbbs2-jpg <kvaraprasadmbbs2@gmail.com>
2026-08-26 18:50:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
-st.title("bmi calculator")
-weight = st.number_input("Enter your Weight in kg",min_value = 1.0)
-height = st.number_input("Enter your height in mt",min_value = 0.5)
-if st.button("calculate bmi"):
-  bmi = weight / (height * height)
-  st.write("Your bmi is = ", round (bmi,2))
-  if bmi < 18.5:
-  st.write("bmi category - Underweight")
-elif bmi < 25:
-  st.write ("bmi categoery - Normal")
-elif bmi <30:
-  st.write("bmi categoery - Overweight ")
-else:
-  st.write("bmi category - Obese")
+
+st.title("BMI Calculator")
+
+weight = st.number_input("Enter your weight in kg", min_value=1.0)
+height = st.number_input("Enter your height in meters", min_value=0.5)
+
+if st.button("Calculate BMI"):
+    bmi = weight / (height * height)
+
+    st.write("Your BMI is =", round(bmi, 2))
+
+    if bmi < 18.5:
+        st.write("BMI Category - Underweight")
+    elif bmi < 25:
+        st.write("BMI Category - Normal")
+    elif bmi < 30:
+        st.write("BMI Category - Overweight")
+    else:
+        st.write("BMI Category - Obese")
